90cee2f67dbf4fb0b31ee792f28a3ab6e679eb94
Riemori <nishishinjuku.rie@gmail.com>
2024-09-11 02:21:21 +0900

    あいさつアプリ課題完了

---
+++ resources/views/message/afternoon.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>afternoon</title>
+</head>
+
+<body>
+    <h1>昼のあいさつ</h1>
+    <h2>こんにちは</h2>
+</body>
+
+</html>
